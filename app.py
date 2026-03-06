bc8d9b26c258de1010f6aba4a56d5c4a2873f416
AjithramAS <ajithram@dilligentech.com>
2026-03-06 11:53:24 +0530

    Fix division by zero and streamline app.py
    Removed division by zero error and cleaned up code.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import sys
-x = 10 / 0
+
 app = Flask(__name__)
 
 @app.get("/")
@@ -9,4 +9,5 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
+
 

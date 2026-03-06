ff62f740e25557f14c2f9f5f255ccc51476ba004
AjithramAS <ajithram@dilligentech.com>
2026-03-06 13:31:11 +0530

    Read secret from environment variable in home route
    Added functionality to read a secret from an environment variable and pass it to the template.

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 import sys
+import os
 
 app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return render_template("index.html", python_version=sys.version)
+    # Read secret from environment variable
+    secret_value = os.getenv("MY_SECRET", "No Secret Found")
+
+    return render_template(
+        "index.html",
+        python_version=sys.version,
+        secret_value=secret_value
+    )
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
-
-
+    app.run(host="0.0.0.0", port=8000)
